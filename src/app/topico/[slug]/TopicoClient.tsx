78290fee8c74b75c65891b5afbdbd30562aa0497
Vinni Cirne <vinnicirne@gmail.com>
2026-09-11 19:23:41 -0300

    style: change like icon to logo_dev

--- src/app/topico/[slug]/TopicoClient.tsx
+++ src/app/topico/[slug]/TopicoClient.tsx
@@ -261,7 +261,7 @@ function CommentItem({
             type="button"
             title={user ? 'Curtir comentário' : 'Faça login para curtir'}
           >
-            <span className="material-symbols-outlined text-[16px]" style={liked ? { fontVariationSettings: "'FILL' 1" } : {}}>thumb_up</span>
+            <span className="material-symbols-outlined text-[16px]" style={liked ? { fontVariationSettings: "'FILL' 1" } : {}}>logo_dev</span>
             <span>{likeCount}</span>
           </button>
 
@@ -452,7 +452,7 @@ export default function TopicoClient({
                 type="button"
                 title={user ? (liked ? 'Remover curtida' : 'Curtir') : 'Faça login para curtir'}
               >
-                <span className="material-symbols-outlined text-[18px]" style={liked ? { fontVariationSettings: "'FILL' 1" } : {}}>favorite</span>
+                <span className="material-symbols-outlined text-[18px]" style={liked ? { fontVariationSettings: "'FILL' 1" } : {}}>logo_dev</span>
                 <span>{likeCount}</span>
               </button>
 
